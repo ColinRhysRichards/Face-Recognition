5495e3d827c221b00c4ca0480eccfc635a01525c
colin richards <colin.rhys.richards@gmail.com>
2020-06-11 23:46:52 -0600

    input function and a button function

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -1,37 +1,70 @@
 import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
-import Particles from 'react-particles-js';
 
-const particleOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+
+const app = new Clarifai.App({
+  apiKey: '7f3778c36ddd4f1c84ad929845592e38'
+})
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
     }
   }
 
-  class App extends React.Component {
-    render() {
-      return (
-        <div className="App">
-          <Particles className='particles'
-            params={particlesOptions}
-          />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm />
-          {/* <FaceRecognition /> */}
-        </div>
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b",
+      "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function (response) {
+          console.log(response)
+        },
+        function (err) {
+          // there was an error
+        }
       );
-    }
   }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Particles className='particles'
+          params={particlesOptions}
+        /> */}
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
 
 export default App;
