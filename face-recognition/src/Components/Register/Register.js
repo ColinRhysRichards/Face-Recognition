cf86ad2321827d69eaba15e754b56be32219ca69
colin richards <colin.rhys.richards@gmail.com>
2020-07-13 23:26:59 -0600

    register.js

--- face-recognition/src/Components/Register/Register.js
+++ face-recognition/src/Components/Register/Register.js
@@ -22,6 +22,7 @@ class Register extends React.Component {
         this.setState({ password: event.target.value })
     }
     render() {
+        const { onRouteChange } = this.props;
         return (
             <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
                 <main className="pa4 black-80">
